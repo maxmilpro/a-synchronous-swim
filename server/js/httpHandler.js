ef4f756bddf6e4d6d43b63f452f0783b4f99e4a9
Max Proano <proanom89@gmail.com>
2020-12-16 20:52:53 -0800

    Partially complete refactoring server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
-
+const messageQueue = require('./messageQueue');
 // Path for the background image ///////////////////////
 module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
-let messageQueue = null;
+
 module.exports.initialize = (queue) => {
   messageQueue = queue;
 };
@@ -16,10 +16,12 @@ module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
   if (req.method === 'GET') {
-    let commands = ['up', 'down', 'left', 'right'];
-    let randomNum = Math.floor(Math.random() * (4));
-    res.end(commands[randomNum]);
-  }else {
+    res.end(messageQueue.dequeue());
+    // let commands = ['up', 'down', 'left', 'right'];
+    // let randomNum = Math.floor(Math.random() * (4));
+    // res.end(commands[randomNum]);
+
+  } else {
     res.end();
   };
 
